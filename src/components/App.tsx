ad04d265eaf3c3d9939dbc58dd6808c5b87e54b9
Manu Phatak <manu.phatak@procore.com>
2020-11-11 18:22:19 -0800

    show movie cards

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,52 @@
 import React, { Key, useEffect, useState } from 'react';
-import logo from './logo.svg';
 import styles from './App.module.scss';
 
 export function App() {
-  const state = useQueryLite([1, 2], TheMoveDBApi.getList);
+  const movieList = useQueryLite([1, 1], TheMoveDBApi.getList);
   // eslint-disable-next-line no-console
-  console.log('state', state);
+  console.log('state', movieList);
+
+  if (movieList.isLoading) {
+    // TODO: Loading screen
+    return <>Loading</>;
+  }
 
   return (
-    <div className={styles.App}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.App}>
+        <input
+          type="search"
+          className={styles.SearchInput}
+          placeholder="Search"
+        />
+
+        {movieList.data.results.map((movie) => (
+          <article key={movie.id} className={styles.MovieCard}>
+            <img
+              className={styles.poster}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt="Poster"
+            />
+
+            <div className={styles.info}>
+              <p className={styles.title}>{movie.title}</p>
+              <p className={styles.overview}>{movie.overview}</p>
+            </div>
+          </article>
+        ))}
+      </div>
     </div>
   );
 }
+
+type MovieList = {
+  results: {
+    id: Key;
+    poster_path: string;
+    title: string;
+    overview: string;
+  }[];
+};
 
 class TheMoveDBApi {
   static requestOptions: RequestInit = {
@@ -37,7 +58,7 @@ class TheMoveDBApi {
     redirect: 'follow',
   };
 
-  static getList(id: Key, page: number) {
+  static getList(id: Key, page: number): Promise<MovieList> {
     const params = new URLSearchParams({ page: page.toString() });
     return fetch(
       `https://api.themoviedb.org/4/list/${id}?${params}`,
