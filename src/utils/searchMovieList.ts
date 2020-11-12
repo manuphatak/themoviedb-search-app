a2b867a45c86e188c81610a88c07451d74029817
Manu Phatak <manu.phatak@procore.com>
2020-11-11 19:29:56 -0800

    reuse lowercased needle

--- src/utils/searchMovieList.ts
+++ src/utils/searchMovieList.ts
@@ -6,7 +6,8 @@ export function searchMovieList<T extends Movie>(
   movieList: T[],
   needle: string
 ): T[] {
+  const lowerCaseNeedle = needle.toLowerCase();
   return movieList.filter((movie) =>
-    movie.overview.toLowerCase().includes(needle.toLowerCase())
+    movie.overview.toLowerCase().includes(lowerCaseNeedle)
   );
 }
