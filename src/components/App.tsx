2f1287f33790707f4c1b336141bc08a4d19a17cc
Manu Phatak <manu.phatak@procore.com>
2020-11-11 17:18:45 -0800

    Fetch list data v1
    - create a hook like useQuery
    - create a TheMoveDBApi to own API access

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Key, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
 
-function App() {
+export function App() {
+  const state = useQueryLite([1, 2], TheMoveDBApi.getList);
+  // eslint-disable-next-line no-console
+  console.log('state', state);
+
   return (
     <div className={styles.App}>
       <header className={styles.header}>
@@ -23,4 +27,44 @@ function App() {
   );
 }
 
-export default App;
+class TheMoveDBApi {
+  static requestOptions: RequestInit = {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json;charset=utf-8',
+      Authorization: `Bearer ${process.env.REACT_APP_THE_MOVIE_DB_BEARER_TOKEN}`,
+    }),
+    redirect: 'follow',
+  };
+
+  static getList(id: Key, page: number) {
+    const params = new URLSearchParams({ page: page.toString() });
+    return fetch(
+      `https://api.themoviedb.org/4/list/${id}?${params}`,
+      TheMoveDBApi.requestOptions
+    ).then((response) => response.json());
+  }
+}
+
+type QueryResponse<T> =
+  | { isLoading: true; data: null }
+  | { isLoading: false; data: T };
+
+function useQueryLite<A extends any[], R>(
+  args: A,
+  query: (...args: A) => Promise<R>
+) {
+  const [state, setState] = useState<QueryResponse<R>>({
+    isLoading: true,
+    data: null,
+  });
+
+  useEffect(() => {
+    // TODO: catch errors
+    query(...args).then((data) => setState({ isLoading: false, data }));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, args);
+
+  return state;
+}
