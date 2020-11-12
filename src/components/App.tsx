b0efacd370c35aae40c875ea8b5057ab81ff8108
Manu Phatak <manu.phatak@procore.com>
2020-11-12 11:15:52 -0800

    Extract LoadedApp component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,47 +11,63 @@ interface AppProps {
 export function App(props: AppProps) {
   const getList = props.getMovieList ?? TheMoveDBApi.getList;
   const movieList = useQueryLite([1, 1], getList);
-  const [searchInput, setSearchInput] = useState('');
   if (movieList.isLoading) {
     // TODO: Loading screen
     return <>Loading</>;
   }
 
-  const searchMovieList = buildMovieListIndex(movieList.data.results);
+  return (
+    <div className={styles.App}>
+      <LoadedApp movies={movieList.data.results} />
+    </div>
+  );
+}
+
+interface LoadedAppProps {
+  movies: {
+    id: React.Key;
+    poster_path: string;
+    title: string;
+    overview: string;
+  }[];
+}
+
+function LoadedApp(props: LoadedAppProps) {
+  const [searchInput, setSearchInput] = useState('');
+
+  const searchMovieList = buildMovieListIndex(props.movies);
 
   const searchResults = searchMovieList(searchInput);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.App}>
-        <input
-          type="search"
-          className={styles.SearchInput}
-          placeholder="Search"
-          value={searchInput}
-          onChange={(event) => setSearchInput(event.target.value)}
-          data-testid="search-movies-input"
-        />
+    <div className={styles.LoadedApp}>
+      <input
+        type="search"
+        className={styles.SearchInput}
+        placeholder="Search"
+        value={searchInput}
+        onChange={(event) => setSearchInput(event.target.value)}
+        data-testid="search-movies-input"
+      />
 
-        {searchResults.map((movie) => (
-          <article
-            key={movie.id}
-            className={styles.MovieCard}
-            data-testid="movie-card"
-          >
-            <img
-              className={styles.poster}
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt="Poster"
-            />
+      {searchResults.map((movie) => (
+        <article
+          key={movie.id}
+          className={styles.MovieCard}
+          data-testid="movie-card"
+        >
+          <img
+            className={styles.poster}
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt="Poster"
+          />
 
-            <div className={styles.info}>
-              <h2 className={styles.title}>{movie.title}</h2>
-              <p className={styles.overview}>{movie.overview}</p>
-            </div>
-          </article>
-        ))}
-      </div>
+          <div className={styles.info}>
+            <h2 className={styles.title}>{movie.title}</h2>
+            <p className={styles.overview}>{movie.overview}</p>
+          </div>
+        </article>
+      ))}
     </div>
   );
 }
